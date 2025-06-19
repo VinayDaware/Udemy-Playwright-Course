d2d25e5d70932bfbbd5e9ac38cea512745c3347e
unknown <dawarevinay@gmail.com>
2025-06-19 20:01:28 +0530

    19th june 2025

--- Playwright Tutorial/tests/dynamicWaitTechnique.spec.js
+++
@@ -1,6 +0,0 @@
-import { test } from '@playwright/test';
-
-test('How to apply dynamic wait ', async function ({ page }) {
-    await page.goto("");
-
-});
--- Playwright Tutorial/tests/signUpPage.spec.js
+++ Playwright Tutorial/tests/signUpPage.spec.js
@@ -1,14 +1,8 @@
-
-const { test, expect } = require('@playwright/test');
-
-test('Launching gmail website and validationg page title', async function ({ page }) {
-
 const {test, expect} = require ('@playwright/test');
 
 test('Launching gmail website and validationg page title',async function({page})
 
-
-
+{
     const url = 'https://rahulshettyacademy.com/loginpagePractise/';
 
     await page.goto(url);
@@ -18,20 +12,13 @@ test('Launching gmail website and validationg page title',async function({page})
     console.log(`The pahe title is ${pageTitle}`);
 
     // validating page title using assertion
-
-    await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
-
-    // locators supported by playwright 1. css and highly reccomended
-    await page.locator('[name = "username"]').fill("vishal");
-    await page.locator('#password').fill("Abc@123");
-    await page.locator('[type = "submit"]').click();
-
    await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
 
    // locators supported by playwright 1. css and highly reccomended
    await page.locator('[name = "username"]').fill("vishal");
    await page.locator('#password').fill("Abc@123");
    await page.locator('[type = "submit"]').click();
+
 
 
 
