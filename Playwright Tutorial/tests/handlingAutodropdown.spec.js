5c26c841ebd0492b4f8261542d9ac8b58d6de058
VinayDaware <dawarevinay@gmail.com>
2025-06-30 22:51:04 +0530

    after putting assertion on table

--- Playwright Tutorial/tests/handlingAutodropdown.spec.js
+++ Playwright Tutorial/tests/handlingAutodropdown.spec.js
@@ -1,5 +1,5 @@
-import {test,expect} from '@playwright/test';
-test('Login with valid credentials,adding item to cart and validating item in cart and checkout',async function({page}){
+import { test, expect } from '@playwright/test';
+test('Login with valid credentials,adding item to cart and validating item in cart and checkout', async function ({ page }) {
     const url = 'https://rahulshettyacademy.com/client/';
     await page.goto(url);
 
@@ -20,11 +20,11 @@ test('Login with valid credentials,adding item to cart and validating item in ca
     console.log(productsCount);
     const productTitles = await page.locator('div.card-body b').allTextContents();
 
-    for (let i =0;i<productsCount;i++){
+    for (let i = 0; i < productsCount; i++) {
         // iterating over the list of products
         const product = await products.nth(i).locator('b').textContent();
 
-        if(product==productName){
+        if (product == productName) {
             // add to cart 
             await products.nth(i).locator("text= Add To Cart").click();
             break;
@@ -40,8 +40,8 @@ test('Login with valid credentials,adding item to cart and validating item in ca
     // asertion for checking correct item added to cart
 
     await page.locator("div li").first().waitFor(); // we use wait here because isVisible method does not support auto wait mechanism
-    const checkProduct =  await page.locator("h3:has-text('ADIDAS ORIGINAL')").isVisible();   
- 
+    const checkProduct = await page.locator("h3:has-text('ADIDAS ORIGINAL')").isVisible();
+
     // h3 is tag name and identifying the product name based on that,isVisible method is check whether the element is Visible
     await expect(checkProduct).toBeTruthy();
 
@@ -69,7 +69,7 @@ test('Login with valid credentials,adding item to cart and validating item in ca
 
     // Autosuggestive country drop down handling
     const countryBox = page.locator('[placeholder*="Country"]');
-    await countryBox.pressSequentially('Ind',{delay:100}); // pressSequentially method is used to fill the text one by one letter in the text box. You only need to press keys one by one if there is special keyboard handling on the page.
+    await countryBox.pressSequentially('Ind', { delay: 100 }); // pressSequentially method is used to fill the text one by one letter in the text box. You only need to press keys one by one if there is special keyboard handling on the page.
 
     // saving list of visible options after entering chracters into the country box in options variable
     const countryDropdown = page.locator('.ta-results');
@@ -79,17 +79,16 @@ test('Login with valid credentials,adding item to cart and validating item in ca
     const optionsCount = await countryDropdown.locator('button').count();  // getting count of the visible options in dropdown
 
     // iterating over each and every option from country list
-    for(let i = 0;i<optionsCount;i++ ){
-      const text = await countryDropdown.locator('button').nth(i).textContent();
-    
-      if(text===' India')
-      {
-        // if the text matches the India it will click on India
-        //click on matching text
-        await countryDropdown.locator('button').nth(i).click();
-        break;
+    for (let i = 0; i < optionsCount; i++) {
+        const text = await countryDropdown.locator('button').nth(i).textContent();
 
-      }
+        if (text === ' India') {
+            // if the text matches the India it will click on India
+            //click on matching text
+            await countryDropdown.locator('button').nth(i).click();
+            break;
+
+        }
     }
     //validating login id is displayed on email id box
     const actualEmail = page.locator(".user__name label[type='text']");
@@ -111,22 +110,26 @@ test('Login with valid credentials,adding item to cart and validating item in ca
     // checking orderID in orders table
     const ordersBtn = page.locator('button[routerlink*="myorders"]');
     await ordersBtn.click();
+    await page.locator('tbody').waitFor();  // it will wait until full table body is loaded
 
     const orderRows = page.locator('tbody tr');   // I used the rows because it consist all the data about the product sp it is easy to extract name and all
     const rowsCount = await orderRows.count();
-    for(let i =0 ;i<rowsCount;i++)
-    {
-        const actualOrderID = await orderRows.nth(i).locator('th').textContent();     // it retrives only in only order ID column and get the text
-        
 
-        if(orderID.includes(actualOrderID))  // here I use includes because in OrderID it retrives | | with orderID
+    for (let i = 0; i < rowsCount; i++) {
+        const actualOrderID = await orderRows.nth(i).locator('th').textContent();     // it retrives only in only order ID column and get the text
+
+
+        if (orderID.includes(actualOrderID))  // here I use includes because in OrderID it retrives | | with orderID
         {
             const viewBtn = orderRows.nth(i).locator('td button.btn-primary');
             await viewBtn.click();
             break;
 
         }
-
     }
+
+    // here we did not put any external wait as the textContent() has auto wait capabilities
+    const orderIDDetails = await page.locator('.col-text').textContent();
+    expect(orderID.includes(orderIDDetails)).toBeTruthy();
 
 });
