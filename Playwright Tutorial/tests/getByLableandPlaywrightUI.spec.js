c2a468cf597bfabaddd0faafd8cf9b87079aa3c8
unknown <dawarevinay@gmail.com>
2025-07-01 19:13:01 +0530

    element filtering using getBYLabel,GetByText getByPlaceholder 01 july 2025

---
+++ Playwright Tutorial/tests/getByLableandPlaywrightUI.spec.js
@@ -0,0 +1,41 @@
+import {test} from '@playwright/test';
+
+test('Playwright special locators',async function({page}){
+    const url = 'https://rahulshettyacademy.com/angularpractice/';
+    await page.goto(url);
+    const nameField = page.locator('[name="name"]'); 
+    await nameField.first().fill('Vinay');
+
+    const emailField = page.locator('[name="email"]');
+    await emailField.fill('dawarevinay@gmail.com');
+
+    const passField = page.locator('[type="password"]');
+    await passField.fill('Test@1234');
+
+    // getByLabel() is locating methos in playwright testing library selectors it is used to find element usually by the visible label
+    const loveIceCreamcheckbox = page.getByLabel('Check me out if you Love IceCreams!'); // here 'Check me out if you Love IceCreams!' is the label name
+    await loveIceCreamcheckbox.click();
+
+    const genderDrown = page.getByLabel('Gender');
+    await genderDrown.selectOption('Female');
+
+    const employmentStatus = page.getByLabel('Employed');
+    await employmentStatus.click();
+
+    const bday = page.locator('[name="bday"]');
+    await bday.pressSequentially('26061995');
+
+
+
+
+
+
+
+    // command for running test using playwright interface is ---> npx playwright test fileName --ui
+
+
+
+
+
+
+});
---
+++ Playwright Tutorial/tests/getByRoleandGetByText.spec.js
@@ -0,0 +1,86 @@
+import{test} from '@playwright/test';
+test('Performing test using get by role locator',async function({page}){
+
+    const url = 'https://rahulshettyacademy.com/angularpractice/';
+    await page.goto(url);
+    const nameField = page.locator('[name="name"]'); 
+    await nameField.first().fill('Vinay');
+
+    const emailField = page.locator('[name="email"]');
+    await emailField.fill('dawarevinay@gmail.com');
+
+    const passField = page.locator('[type="password"]');
+    await passField.fill('Test@1234');
+
+    // getByLabel() is locating methos in playwright testing library selectors it is used to find element usually by the visible label
+    const loveIceCreamcheckbox = page.getByLabel('Check me out if you Love IceCreams!'); // here 'Check me out if you Love IceCreams!' is the label name
+    await loveIceCreamcheckbox.click();
+
+    const genderDrown = page.getByLabel('Gender');
+    await genderDrown.selectOption('Female');
+
+    const employmentStatus = page.getByLabel('Employed');
+    await employmentStatus.click();
+
+    const bday = page.locator('[name="bday"]');
+    await bday.pressSequentially('26061995');
+
+    //getByRole() method is used to identify the element using its accessibility role
+    const submitBtn = page.getByRole("button",{name : 'Submit'});
+    await submitBtn.click();
+
+
+});
+
+
+test('Performing test using get by text locator',async function({page}){
+    const url = 'https://rahulshettyacademy.com/angularpractice/';
+    await page.goto(url);
+        const url = 'https://rahulshettyacademy.com/angularpractice/';
+    await page.goto(url);
+    const nameField = page.locator('[name="name"]'); 
+    await nameField.first().fill('Vinay');
+
+    const emailField = page.locator('[name="email"]');
+    await emailField.fill('dawarevinay@gmail.com');
+
+    const passField = page.locator('[type="password"]');
+    await passField.fill('Test@1234');
+
+    // getByLabel() is locating methos in playwright testing library selectors it is used to find element usually by the visible label
+    const loveIceCreamcheckbox = page.getByLabel('Check me out if you Love IceCreams!'); // here 'Check me out if you Love IceCreams!' is the label name
+    await loveIceCreamcheckbox.click();
+
+    const genderDrown = page.getByLabel('Gender');
+    await genderDrown.selectOption('Female');
+
+    const employmentStatus = page.getByLabel('Employed');
+    await employmentStatus.click();
+
+    const bday = page.locator('[name="bday"]');
+    await bday.pressSequentially('26061995');
+
+    const submitBtn = page.getByRole("button",{name : 'Submit'});
+    await submitBtn.click();
+
+    //getByText() is method used to identify an element based on visible inner text
+    const successMsg = page.getByText(' The Form has been submitted successfully!.');
+   console.log(await successMsg.textContent());
+
+
+});
+
+
+test('Performing test using get by placeholder locator',async function({page}){
+    const url = 'https://rahulshettyacademy.com/angularpractice/';
+    await page.goto(url);
+
+    //getByPlaceholder() method is use to identify an element using placeholder locator
+    const passField = page.getByPlaceholder('Password');
+    await passField.fill('Test@1234');
+
+
+
+});
+
+
