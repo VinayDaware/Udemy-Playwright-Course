d016a78e4d3c78f6eefcfc849a819b5ace15f4af
vinay.daware <dawarevinay@gmail.com>
2026-02-18 18:54:59 +0530

    18 feb 2025 ---> split method,trim method,trimStart metthod,trimEnd method

--- Rahul Shetty JS/strings.js
+++ Rahul Shetty JS/strings.js
@@ -1,5 +1,7 @@
 // Strings and its methods
 
+const { start } = require("node:repl");
+
 // 1. length property --> it returns length of an string. It stores data does not perform any action hence it is known as property.
 const firstName = "Vinay";
 const len = firstName.length;
@@ -23,4 +25,57 @@ console.log(subText2);
 const subText3 = text.slice(-5);
 console.log(subText3);
 
+// 3. charAt() method - This method returns the character at specified index position in a string. 
+const fullName = "Vinay Daware";
+const charPosition = fullName.charAt(4);
+console.log(charPosition);
+
+// 4. split () method - This method is used to divide string into an array of substrings based on specified seperator
+// spliting using  ,
+const fruits = 'apple,banana,mango';
+const splitFruits = fruits.split(',');
+console.log(splitFruits);       // [apple,banana,mango]
+console.log(splitFruits[1]);    // 0 - apple, 1 - banana, 2-mango 
+console.log(splitFruits[1,2])   // banana, mango
+
+// spliting using " "
+const sentence = "Orrange city of India - Nagpur";
+const splitSentence = sentence.split(" ");
+console.log(splitSentence);         // ['Orrange','city', 'of', 'India', '-', 'Nagpur']
+console.log(splitSentence[3]);      // India
+
+// Split using characters ""
+const name = "Vinay";
+const splitName = name.split("");
+console.log(splitName);     //[ 'V', 'i', 'n', 'a', 'y' ]
+console.log(splitName[4]);  // y
+
+// Split using - '-'
+const capital = "New Delhi - is a capital of India";
+const splitCapital = capital.split('-');
+console.log(splitCapital);  // [ 'New Delhi ', ' is a capital of India' ]
+console.log(splitCapital[0]); // New Delhi
+
+// split using character
+const day = "Wednesday";
+const splitDay = day.split("n")
+console.log(splitDay);    // ['Wed','esday']
+console.log(splitDay[0]);
+
+// 5.trim() method - This method is used to remove white spaces from bothe side 
+
+// triming both side of white spaces
+const cityName = '    Nagpur     ';
+const cityTrim = cityName.trim();
+console.log(cityName.length);   // 15
+console.log(cityTrim);          //Nagpur
+console.log(cityTrim.length);   //6
+
+//trimStart() method - this method is used to remove an white spaces from start/leading/left side
+const startTrim = cityName.trimStart();
+console.log(startTrim);         // Nagpur     
+
+//trimEnd() method - This method is used to remove the whites spaces from end/trailing/right side
+const endTrim = cityName.trimEnd();
+console.log(endTrim);
 
